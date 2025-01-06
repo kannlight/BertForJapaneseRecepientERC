ade4da0f69d79e72befff25cb54c412b0f45e9d5
user media <kanlight28@gmail.com>
2025-01-06 19:50:02 +0900

    データ分割やテスト方法を変更

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import random
-# from tqdm import tqdm
+from tqdm import tqdm
 import json
 import os
 import torch
@@ -25,40 +25,39 @@ CATEGORIES = [
 ]
 MAX_LENGTH = 512
 
-def load_data():
-    # データセットの対話データをトークン化して返す
+def tokenize_data(filename):
+    # データセットの対話データをトークン化して返す関数
     dataset_for_loader = []
-    for file in os.listdir('DatasetByLuke'):
-        # データセットから対話データを読み込む
-        data = {}
-        with open('DatasetByLuke/'+file, 'r', encoding='utf-8') as f:
-            data = json.load(f)
+    # データセットから対話データを読み込む
+    data = {}
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
 
-        # 各対話をトークン化して追加
-        if 'data' in data:
-            for talk in data['data']:
-                # 1発話目(受信者の発話)と2発話目(送信者の発話)を取り出す（複数続いたら改行で繋げる）
-                t1 = []
-                t2 = []
-                for utter in talk['talk']:
-                    if utter['type'] == 1:
-                        t1.append(utter['utter'])
-                    if utter['type'] == 2:
-                        t2.append(utter['utter'])
-                text1 = '\n'.join(t1)
-                text2 = '\n'.join(t2)
-                # トークン化
-                token=tokenizer(
-                    text1, text2,
-                    truncation=True,
-                    max_length=MAX_LENGTH,
-                    padding="max_length",
-                )
-                # ラベル付け
-                token['labels'] = talk['label']
-                # バリューをテンソル化して追加
-                token = { k: torch.tensor(v) for k, v in token.items() }
-                dataset_for_loader.append(token)
+    # 各対話をトークン化して追加
+    if 'data' in data:
+        for talk in data['data']:
+            # 1発話目(受信者の発話)と2発話目(送信者の発話)を取り出す（複数続いたら改行で繋げる）
+            t1 = []
+            t2 = []
+            for utter in talk['talk']:
+                if utter['type'] == 1:
+                    t1.append(utter['utter'])
+                if utter['type'] == 2:
+                    t2.append(utter['utter'])
+            text1 = '\n'.join(t1)
+            text2 = '\n'.join(t2)
+            # トークン化
+            token=tokenizer(
+                text1, text2,
+                truncation=True,
+                max_length=MAX_LENGTH,
+                padding="max_length",
+            )
+            # ラベル付け
+            token['labels'] = talk['label']
+            # バリューをテンソル化して追加
+            token = { k: torch.tensor(v) for k, v in token.items() }
+            dataset_for_loader.append(token)
     return dataset_for_loader
 
 class BertForJapaneseRecepientERC(pl.LightningModule):
@@ -85,39 +84,24 @@ class BertForJapaneseRecepientERC(pl.LightningModule):
         val_loss = output.loss
         self.log('val_loss', val_loss)
 
-    # テストデータを受け取って評価指標を計算
-    def test_step(self, batch):
-        # モデルが出力した分類スコアから、最大値となるクラスを取得
-        output =self.model(**batch)
-        predicted_labels = output.logits.argmax(-1)
-        # テストデータのラベル
-        true_labels = batch.pop('labels')
-        # precision, recall, f1, データ数 をクラス毎、ミクロ、マクロ、加重平均で算出
-        self.log('test_report', classification_report(true_labels, predicted_labels, target_names=CATEGORIES))
+    # # テストデータを受け取って評価指標を計算
+    # def test_step(self, batch):
+    #     # モデルが出力した分類スコアから、最大値となるクラスを取得
+    #     output =self.model(**batch)
+    #     predicted_labels = output.logits.argmax(-1)
+    #     # テストデータのラベル
+    #     true_labels = batch.pop('labels')
+    #     # precision, recall, f1, データ数 をクラス毎、ミクロ、マクロ、加重平均で算出
+    #     self.log('test_report', classification_report(true_labels, predicted_labels, target_names=CATEGORIES))
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
 
-
-
-# def test():
-#     dataset_for_loader = load_data()
-#     print(len(dataset_for_loader))
-#     print(dataset_for_loader[0])
-#     print(dataset_for_loader[-1])
-
 def main():
-    # データセットから対話データのデータローダを作成
-    dataset_for_loader = load_data()
-    # 対話データをシャッフル
-    random.shuffle(dataset_for_loader)
-    # 6:2:2で学習:検証:テストデータに分割
-    n = len(dataset_for_loader)
-    n_train = int(0.6*n)
-    n_val = int(0.2*n)
-    dataset_train = dataset_for_loader[:n_train]
-    dataset_val = dataset_for_loader[n_train:n_train+n_val]
-    dataset_test = dataset_for_loader[n_train+n_val:]
+    # データセットから対話データをトークン化
+    dataset_train = tokenize_data('DatasetTrain.json')
+    dataset_val = tokenize_data('DatasetVal.json')
+    dataset_test = tokenize_data('DatasetTest.json')
     # データローダ作成
     dataloader_train = DataLoader(dataset_train, batch_size=32, shuffle=True)
     dataloader_val = DataLoader(dataset_val, batch_size=256)
@@ -135,7 +119,7 @@ def main():
     trainer = pl.Trainer(
         accelerator = 'gpu', # 学習にgpuを使用
         devices = 1, # gpuの個数
-        max_epochs = 10, # 学習のエポック数
+        max_epochs = 5, # 学習のエポック数
         callbacks = [checkpoint]
     )
     # 学習率を指定してモデルをロード
@@ -148,8 +132,13 @@ def main():
     print('val_loss for best_model:', checkpoint.best_model_score)
 
     # テストデータで評価
-    test = trainer.test(dataloaders=dataloader_test)
-    print(test[0]['test_report'])
+    # test = trainer.test(dataloaders=dataloader_test)
+    # print(test[0]['test_report'])
+
+    best_model = BertForJapaneseRecepientERC.load_from_checkpoint(
+        checkpoint.best_model_path
+    )
+    best_model.model.save_pretrained('./model_transformers')
 
 if __name__ == "__main__":
     main()
